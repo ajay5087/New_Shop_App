c17589abf9a3ac48cc1090a9c5a9f0bdb4dc6903
Ajay Verma <vajay5126@gmail.com>
2020-09-11 11:47:25 +0530

    New_Shop_App

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../screens/edit_product_screen.dart';
+import 'package:myshop/providers/products.dart';
+import 'package:provider/provider.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  UserProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    return ListTile(
+      title: Text(
+        title,
+      ),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Container(
+        width: 100.0,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.edit,
+                color: Theme.of(context).primaryColor,
+              ),
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.delete,
+                color: Theme.of(context).errorColor,
+              ),
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (error) {
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Deleting Failed',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
